269fe050cd9fd43f0775a63b9a7610484e7943af
2002pratham <prathams1334@gmail.com>
2025-11-01 00:15:36 +0530

    Updated Master Branch

--- src/main/java/com/igreaper/learning/Introduction/IntroductionToSpringbootApplication.java
+++ src/main/java/com/igreaper/learning/Introduction/IntroductionToSpringbootApplication.java
@@ -6,17 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroductionToSpringbootApplication implements CommandLineRunner {
-
-    @Autowired
-    Student student;
+public class IntroductionToSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroductionToSpringbootApplication.class, args);
+        System.out.println("Please lookout in branches for each topics individually");
 	}
-
-    @Override
-    public void run(String... args) throws Exception {
-        student.intro();
-    }
 }
